a281866909a6bb8f81afe1f829eb5b72bc2354dd
Ibrahim Al-Bari <ibrahim2010963@gmail.com>
2026-01-17 13:27:50 +0300

    Fixed import paths

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GameP/',
+  base: '/',
   resolve: { dedupe: ['react', 'react-dom'] },
   server: {
     proxy: {
